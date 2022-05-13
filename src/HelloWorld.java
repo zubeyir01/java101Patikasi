e7b73e4afb61f26cc4e93137dd7d19acecf208ef
Zubeyir Ozdemir <zubeyirozdemir1@gmail.com>
2022-05-13 20:12:50 +0300

    not ortalamasina göre sınıf geçme kalma durumunu belirten program yazıldı.

---
+++ src/HelloWorld.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+    Scanner s = new Scanner(System.in);
+    int mat,fizik,kimya,turkce,tarih,muzik;
+
+            System.out.print("Matematik notunuzu giriniz:");
+            mat=s.nextInt();
+
+            System.out.print("Fizik notunuzu giriniz:");
+            fizik=s.nextInt();
+
+            System.out.print("Kimya notunuzu giriniz:");
+            kimya=s.nextInt();
+
+            System.out.print("Turkce notunuzu giriniz:");
+            turkce=s.nextInt();
+
+            System.out.print("Tarih notunuzu giriniz:");
+            tarih=s.nextInt();
+
+            System.out.print("Muzik notunuzu giriniz:");
+            muzik=s.nextInt();
+
+            double ort = (mat+fizik+kimya+turkce+tarih+muzik)/6;
+            System.out.println("Ortalamaniz:"+ort);
+
+            String basari =  (ort>60) ? "Sinifi gectiniz!":"Sinifta kaldiniz!";
+            System.out.println(basari);
+
+    }
+}
