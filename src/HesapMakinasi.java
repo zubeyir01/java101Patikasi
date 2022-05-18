4feb070431847755a90b0cfd36a117af4d1b57ac
Zubeyir Ozdemir <zubeyirozdemir1@gmail.com>
2022-05-19 00:30:14 +0300

    Hesap makinasi odev eklendi

---
+++ src/HesapMakinasi.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class HesapMakinasi {
+    public static void main(String[] args) {
+        int n1,n2,select;
+        Scanner input = new Scanner(System.in);
+        System.out.println("1-Toplama\n2-Cikarma\n3-Carpma\n4-Bolme");
+        System.out.print("Seciminiz:");
+        select = input.nextInt();
+        System.out.print("Ilk sayiyi giriniz:");
+        n1 = input.nextInt();
+        System.out.print("Ikinci sayiyi giriniz:");
+        n2 = input.nextInt();
+        switch (select) {
+            case 1:
+                System.out.println("Sonuc:" + (n1 + n2));
+                break;
+            case 2:
+                System.out.println("Sonuc:" + (n1 - n2));
+                break;
+            case 3:
+                System.out.println("Sonuc:" + (n1 * n2));
+                break;
+            case 4:
+                System.out.println("Sonuc:" + (n1 / n2));
+                break;
+            default:
+                System.out.println("Dogru secim yapmadiniz!");
+        }
+    }
+}
