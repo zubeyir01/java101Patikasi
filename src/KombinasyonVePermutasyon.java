676e1e4671dec3300c0e94c56cf48e5d95aa83ec
Zubeyir Ozdemir <zubeyirozdemir1@gmail.com>
2022-05-24 01:33:02 +0300

    Kombinasyon ve permutasyon hesaplama odevı

--- src/KombinasyonVePermutasyon.java
+++ src/KombinasyonVePermutasyon.java
@@ -5,23 +5,24 @@ public class KombinasyonVePermutasyon {
     static int factorial(int n) {
         int fact = 1;
         int i = 1;
-        while(i <= n) {
+        while (i <= n) {
             fact *= i;
             i++;
         }
         return fact;
     }
+
     public static void main(String[] args) {
         // C(n,r) = n!/ ( r! * (n-r)!)
-        int n,r,per,comb;
+        int n, r, per, comb;
         Scanner s = new Scanner(System.in);
         System.out.print("Ilk sayi:");
         n = s.nextInt();
         System.out.print("Ikinci sayi:");
         r = s.nextInt();
-        per = factorial(n) / factorial(n-r);
-        System.out.println("P(" + n + ","+ r + "):" + per);
-        comb = factorial(n) / (factorial(r) * factorial(n-r));
-        System.out.println("C(" + n + ","+ r + "):" + comb);
+        per = factorial(n) / factorial(n - r);
+        System.out.println("P(" + n + "," + r + "):" + per);
+        comb = factorial(n) / (factorial(r) * factorial(n - r));
+        System.out.println("C(" + n + "," + r + "):" + comb);
     }
 }
